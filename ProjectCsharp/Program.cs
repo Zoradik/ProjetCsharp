9cd675ec11a33e542d23dc7ed5e097f3a2b8cfd3
Zoradik <56223677+Zoradik@users.noreply.github.com>
2022-11-19 06:43:37 +0100

    Version de base

--- ProjectCsharp/Program.cs
+++ ProjectCsharp/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class App 
+{
+    static void Main(string [] args)
+    {
+        Console.Write("Bonjour \n(avec le ton de ragnar sinon pas drole"); // \n correspond au retour a la ligne, sinon WriteLine le fait aussi mais deux lignes a faire dans ce cas \t correspond a un alinéas
+    }
+}
+
+
