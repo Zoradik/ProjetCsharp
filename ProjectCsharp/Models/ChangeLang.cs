8f85af252ce3236ff58ea95835d2c6947f3fc693
Zoradik <56223677+Zoradik@users.noreply.github.com>
2022-12-13 22:18:11 +0100

    ajouts de charactères langues

--- ProjectCsharp/Models/ChangeLang.cs
+++ ProjectCsharp/Models/ChangeLang.cs
@@ -6,74 +6,69 @@ namespace test2
 {
     class ChangeLang
     {
-        public string[] englishTranslation = {
-            "Add Save Work",
-            "Run Save Work",
-            "Settings",
-            "The best backup management software you can have !",
-            "The best backup management software you can have !",
-            "The best backup management software you can have !",
-            "Save work name :",
-            "Source path :",
-            "Target path :",
-            "Save work type :",
-            "Full",
-            "Differential",
-            "Files encrypted :",
-            "Crypted",
-            "Add",
-            "Start Sequential Run",
-            "Number",
-            "Start Single Run",
-            "Progress",
-            "Pause",
-            "Stop",
-            "French",
-            "English",
-            "Open User Guide",
-            "Open Config",
-            "Open Logs",
-        };
-        public string[] frenchTranslation = {
-            "Ajouter un travail de sauvegarde",
-            "Executer un travail de sauvegarde",
-            "Paramètres",
-            "Le meilleur logiciel de gestion de sauvegarde que vous puissiez avoir !",
-            "Le meilleur logiciel de gestion de sauvegarde que vous puissiez avoir !",
-            "Le meilleur logiciel de gestion de sauvegarde que vous puissiez avoir !",
-            "Nom du TS :",
-            "Chemin source :",
-            "Chemin cible :",
-            "Type de TS :",
-            "Complet",
-            "Différentiel",
-            "Fichiers chiffrés :",
-            "Chiffrés",
-            "Ajouter",
-            "Lancement séquentiel",
-            "Nombre",
-            "Lancement unique",
-            "Progression",
-            "Pause",
-            "Stop",
-            "Français",
-            "Anglais",
-            "Ouvrir le guide utilisateur",
-            "Ouvrir la configuration",
-            "Ouvrir les logs"
-        };
+        public string
+            printNoSaveWorkFound = "No backup job found with entry",
+            printImpossibleToRunBuissnessSoftwareRunning = "Impossible to run the save work, a buisness software is running. ",
+            printSaveWorkAlreadyExist = "A save work with the same name already exist",
+            printSaveWorkAdded = "Save work successfully added",
+            printError = "An error has occured",
+            printFillOut = "please complete all fields",
+            printExecutedworks = "all save works has been executed",
+            printHasBeenExecuted = "has been successfully executed",
+            printEmptyTargetDir = "The specified destination directory is empty",
+            printEmptySourceDir = "The specified source directory is empty",
+            printPaused = "You paused the save work(s)",
+            printStop = "You Stopped the save work(s)",
+            printResume = "You resumed the save work(s)";
 
-        public void changeLangFunction(Object[] content, string[] translation)
+
+
+        public string changeMessageBoxLang(string lang)
         {
-            if (content.Length == translation.Length)
+            if (lang == "french")
             {
-                for (int i = 0; i < content.Length; i++)
-                {
-                    content = translation;
-                }
-            }
-        }
+                printNoSaveWorkFound = "Pas de travail de sauvegarde trouvé avecc cette entrée ";
+                printImpossibleToRunBuissnessSoftwareRunning = "Impossible de lancer car un logiciel métier est en cours d'éxecution";
+                printSaveWorkAlreadyExist = "Un travail avec le même nom existe déjà !";
+                printSaveWorkAdded = "Travail ajouté avec succès !";
 
+                printError = "Une erreur est survenue";
+                printFillOut = "Veuillez remplir tous les champs";
+                printExecutedworks = "Tous les travaux sont executées ";
+                printHasBeenExecuted = "à été executé avec succes";
+                printEmptyTargetDir = "Le répertoire de destination précisé est vide";
+                printEmptySourceDir = "Le répertoire source précisé est vide";
+                printPaused = "Vous avez mis en pause le ou les travaux de sauvegarde";
+                printStop = "Vous avez arrêté le ou les travaux de sauvegarde";
+                printResume = "Vous avez repris le ou les travaux de sauvegarde";
+
+                return "Language changé vers français avec succès";
+            }
+            if (lang == "english")
+            {
+                printNoSaveWorkFound = "No backup job found with entry";
+                printImpossibleToRunBuissnessSoftwareRunning = "Impossible to run the save work, a buisness software is running. ";
+                printSaveWorkAlreadyExist = "A save work with the same name already exist";
+                printSaveWorkAdded = "Save work successfully added";
+
+                printError = "An error has occured";
+                printFillOut = "please complete all fields";
+                printExecutedworks = "all save works has been executed";
+                printHasBeenExecuted = "has been successfully executed";
+                printEmptyTargetDir = "The specified destination directory is empty";
+                printEmptySourceDir = "The specified source directory is empty";
+                printPaused = "You paused the save work(s)";
+                printStop = "You Stopped the save work(s)";
+                printResume = "You resumed the save work(s)";
+
+                return "Language successfully changed to english";
+            }
+            else
+            {
+                return "error";
+            }
+
+        }
 
     }
 }
