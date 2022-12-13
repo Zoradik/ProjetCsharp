2f6e1dc1e4ea33ac9b07b89bef7d359d02079dbe
Zoradik <56223677+Zoradik@users.noreply.github.com>
2022-12-13 22:09:14 +0100

    WPF

--- ProjectCsharp/Views/MainWindow.xaml.cs
+++ ProjectCsharp/Views/MainWindow.xaml.cs
@@ -16,14 +16,16 @@ using System.IO;
 
 namespace test2
 {
-    // <summary>
-    /// Logique d'interaction pour MainWindow.xaml
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        ChangeLang lang = new ChangeLang();
+
         public MainWindow()
         {
-          InitializeComponent();
+            //InitializeComponent();
         }
 
 
@@ -33,7 +35,7 @@ namespace test2
 
             if (tab1TextBoxName.Text != "" && tab1TextBoxSourcePath.Text != "" && tab1TextBoxTargetPath.Text != "" && tab1SelectType.Text != "")
             {
-                Projet.EasySave addWork = new Projet.EasySave();
+                test2.EasySave addWork = new test2.EasySave();
 
                 int fCount = Directory.GetFiles(tab1TextBoxSourcePath.Text, "*", SearchOption.AllDirectories).Length;
                 long size = addWork.GetFileSizeSumFromDirectory(tab1TextBoxSourcePath.Text);
@@ -47,12 +49,12 @@ namespace test2
                     tab1SelectType.Text = "";
                     tab1SaveWork_Loaded(sender, e);
 
-                } 
+                }
                 catch
                 {
-                   
-                   
-                        MessageBox.Show("une erreur est survenue");
+
+
+                    MessageBox.Show(lang.printError);
 
 
                     tab1TextBoxName.Text = "";
@@ -63,101 +65,103 @@ namespace test2
             }
             else
             {
-               
-               
-               
-                   MessageBox.Show("Veuillez remplir tout les champs");
-                
+
+
+
+                MessageBox.Show(lang.printFillOut);
+
             }
 
         }
 
-
-        //executer save work
+        public int pgBarValue = 0;
+        //tab2 run save work
         private void tab2ButtonStartSequentialRun_Click(object sender, RoutedEventArgs e)
         {
-            Projet.EasySave exeseqWork = new Projet.EasySave();
+            pgBarValue = 0;
+            test2.EasySave exeseqWork = new test2.EasySave();
             try
             {
                 exeseqWork.ExecuteAllWork();
+                tab2ProgessBar.Value = exeseqWork.change_pgBarValue();
 
-               
+                MessageBox.Show(lang.printExecutedworks);
+                tab2DataGrid_Loaded(sender, e);
+                tab1SaveWork_Loaded(sender, e);
 
             }
             catch
             {
-
-
-                MessageBox.Show("une erreur est survenue");
-
-
+                MessageBox.Show(lang.printError);
             }
         }
 
         private void tab2ButtonStartSingleRun_Click(object sender, RoutedEventArgs e)
         {
+            pgBarValue = 0;
             if (tab2TextBoxNumber.Text != "")
             {
-                Projet.EasySave exeWork = new Projet.EasySave();
+                test2.EasySave exeWork = new test2.EasySave();
 
-               
+                exeWork.ExecuteWork(tab2TextBoxNumber.Text);
+                pgBarValue += 100;
+                tab2ProgessBar.Value = pgBarValue;
 
-                try
-                {
-                    exeWork.ExecuteWork(tab2TextBoxNumber.Text);
-
-                    MessageBox.Show("travail" + tab2TextBoxNumber.Text + "à été executé avec succes");
-
-                }
-                catch
-                {
-
-
-                    MessageBox.Show("une erreur est survenue");
-
-
-                }
+                tab1SaveWork_Loaded(sender, e);
+                MessageBox.Show(tab2TextBoxNumber.Text + lang.printHasBeenExecuted);
             }
             else
             {
-
-
-
-                MessageBox.Show("Veuillez remplir tout les champs");
+                MessageBox.Show(lang.printFillOut);
 
             }
         }
 
         private void tab2ButtonPause_Click(object sender, RoutedEventArgs e)
         {
-
+            EasySave easySave = new EasySave();
+            easySave.pauseThread();
+            MessageBox.Show(lang.printPaused);
         }
 
         private void tab2ButtonStop_Click(object sender, RoutedEventArgs e)
         {
-
+            EasySave easySave = new EasySave();
+            easySave.stopThread();
+            pgBarValue = 0;
+            MessageBox.Show(lang.printStop);
         }
 
-        //tab3 paramètres
+        private void tab2ButtonResume_Click(object sender, RoutedEventArgs e)
+        {
+            EasySave easySave = new EasySave();
+            easySave.resumeThread();
+            MessageBox.Show(lang.printResume);
+        }
+
+        //tab3 Settings 
 
         private void tab3ButtonFrench_Click(object sender, RoutedEventArgs e)
         {
-            
+
             Button clickedButton = (Button)sender;
-            //Contenu des bouttons
+            //clickedButton.Content = "...button clicked...";
             clickedButton.IsEnabled = false;
             tab3ButtonEnglish.IsEnabled = true;
 
             tab1SaveWork.Header = "Ajouter un travail de sauvegarde";
             tab2RunWork.Header = "Executer un travail de sauvegarde";
             tab3Settings.Header = "Paramètres";
+            //tab1TextBlockDescription.Text = "Fait ca vite !";
+            //tab2TextBlockDescription.Text = "Fait ca vite !";
+            //tab3TextBlockDescription.Text = "Fait ca vite !";
             tab1LabelName.Content = "Nom du TS :";
             tab1LabelSourcePath.Content = "Chemin source :";
             tab1LabelTargetPath.Content = "Chemin cible :";
             tab1LabelType.Content = "Type de TS :";
             tab1SelectTypeFull.Content = "Complet";
             tab1SelectTypeDifferential.Content = "Différentiel";
-            tab1LabelCrypted.Content = "Fichiers chiffrés :";
+            tab1LabelCrypted.Content = "Chiffrement ?";
             tab1CheckCrypted.Content = "Chiffrés";
             tab1ButtonAdd.Content = "Ajouter";
             tab2ButtonStartSequentialRun.Content = "Lancement séquentiel";
@@ -166,23 +170,30 @@ namespace test2
             tab2LabelProgessBar.Content = "Progression";
             tab2ButtonPause.Content = "Pause";
             tab2ButtonStop.Content = "Stop";
+            tab2ButtonResume.Content = "Reprendre";
             tab3ButtonFrench.Content = "Français";
             tab3ButtonEnglish.Content = "Anglais";
             tab3ButtonUserGuide.Content = "Ouvrir le guide utilisateur";
             tab3ButtonOpenConfig.Content = "Ouvrir la configuration";
             tab3ButtonOpenLogs.Content = "Ouvrir les logs";
+            tab3ButtonConfigureBuisnessSoftware.Content = "Configurer le Logiciel Métier";
+            tab3ButtonConfigureTypeFilesToEncrypt.Content = "Configurer le type de fichier à chiffrer";
 
+            MessageBox.Show(lang.changeMessageBoxLang("french"));
         }
         private void tab3ButtonEnglish_Click(object sender, RoutedEventArgs e)
         {
             Button clickedButton = (Button)sender;
-            //Contenu des boutons en anglais
+            //clickedButton.Content = "...button clicked...";
             clickedButton.IsEnabled = false;
             tab3ButtonFrench.IsEnabled = true;
-            
+
             tab1SaveWork.Header = "Add Save Work";
             tab2RunWork.Header = "Run Save Work";
             tab3Settings.Header = "Settings";
+            //tab1TextBlockDescription.Text = "Make it quick !";
+            tab2TextBlockDescription.Text = "Make it quick !";
+            tab3TextBlockDescription.Text = "Make it quick !";
             tab1LabelName.Content = "Save work name :";
             tab1LabelSourcePath.Content = "Source path :";
             tab1LabelTargetPath.Content = "Target path :";
@@ -198,45 +209,22 @@ namespace test2
             tab2LabelProgessBar.Content = "Progress";
             tab2ButtonPause.Content = "Pause";
             tab2ButtonStop.Content = "Stop";
+            tab2ButtonResume.Content = "Resume";
             tab3ButtonFrench.Content = "French";
             tab3ButtonEnglish.Content = "English";
             tab3ButtonUserGuide.Content = "Open User Guide";
             tab3ButtonOpenConfig.Content = "Open Config";
             tab3ButtonOpenLogs.Content = "Open Logs";
-            
+            tab3ButtonConfigureBuisnessSoftware.Content = "Configure Buisness Software";
+            tab3ButtonConfigureTypeFilesToEncrypt.Content = "Configure Type of Files to Encrypt";
 
-            Object[] contentToTranslate = {
-                tab1SaveWork.Header,
-                tab2RunWork.Header,
-                tab3Settings.Header,
-                tab1LabelName.Content,
-                tab1LabelSourcePath.Content,
-                tab1LabelTargetPath.Content,
-                tab1LabelType.Content,
-                tab1SelectTypeFull.Content,
-                tab1SelectTypeDifferential.Content,
-                tab1LabelCrypted.Content,
-                tab1CheckCrypted.Content,
-                tab1ButtonAdd.Content,
-                tab2ButtonStartSequentialRun.Content,
-                tab2TextBoxNumber.Text,
-                tab2ButtonStartSingleRun.Content,
-                tab2LabelProgessBar.Content,
-                tab2ButtonPause.Content,
-                tab2ButtonStop.Content,
-                tab3ButtonFrench.Content,
-                tab3ButtonEnglish.Content,
-                tab3ButtonUserGuide.Content,
-                tab3ButtonOpenConfig.Content,
-                tab3ButtonOpenLogs.Content
-            };
+            MessageBox.Show(lang.changeMessageBoxLang("english"));
 
-            ChangeLang test = new ChangeLang();
-            test.changeLangFunction(contentToTranslate, test.englishTranslation);
+
         }
         private void tab3ButtonUserGuide_Click(object sender, RoutedEventArgs e)
         {
-            OpenProcess.OpenProcessFunction("notepad.exe", @"..\..\..\Files\README_EN.md");
+            OpenProcess.OpenProcessFunction("notepad.exe", @"..\..\..\Files\UserGuide.txt");
         }
 
         private void tab3ButtonOpenConfig_Click(object sender, RoutedEventArgs e)
@@ -251,9 +239,9 @@ namespace test2
 
         }
 
-        
 
-        //Fermer les botuons
+
+        //button close
         private void CloseButton_Click(object sender, RoutedEventArgs e)
         {
             Close();
@@ -261,53 +249,53 @@ namespace test2
 
         private void TargetPATH_Clicked(object sender, RoutedEventArgs e)
         {
-            // Créer OpenFileDialog
+            // Create OpenFileDialog
             Ookii.Dialogs.Wpf.VistaFolderBrowserDialog openDlg = new Ookii.Dialogs.Wpf.VistaFolderBrowserDialog();
 
-            // Lancer OpenFileDialog en appelant la méthode ShowDialog
+            // Launch OpenFileDialog by calling ShowDialog method
             Nullable<bool> result = openDlg.ShowDialog();
-            //Récupérer le nom du fichier sélectionné et l'afficher dans un TextBox.
-            // Charger le contenu du fichier dans un TextBlock
+            // Get the selected file name and display in a TextBox.
+            // Load content of file in a TextBlock
             if (result == true)
             {
                 tab1TextBoxTargetPath.Text = openDlg.SelectedPath;
-                
+                // TextBlock1.Text = System.IO.File.ReadAllText(openFileDlg.FileName);
             }
             else
             {
-                MessageBox.Show("Le répertoire de destination précisé est vide");
+                MessageBox.Show(lang.printEmptyTargetDir);
             }
 
         }
         private void SourcePath_Clicked(object sender, RoutedEventArgs e)
         {
-            // Créer OpenFileDialog
+            // Create OpenFileDialog
             Ookii.Dialogs.Wpf.VistaFolderBrowserDialog openDlg = new Ookii.Dialogs.Wpf.VistaFolderBrowserDialog();
 
-            // Lancer OpenFileDialog en appelant la méthode ShowDialog
+            // Launch OpenFileDialog by calling ShowDialog method
             Nullable<bool> result = openDlg.ShowDialog();
-            //Récupérer le nom du fichier sélectionné et l'afficher dans un TextBox.
-            // Charger le contenu du fichier dans un TextBlock
+            // Get the selected file name and display in a TextBox.
+            // Load content of file in a TextBlock
             if (result == true)
             {
                 tab1TextBoxSourcePath.Text = openDlg.SelectedPath;
-                
+                // TextBlock1.Text = System.IO.File.ReadAllText(openFileDlg.FileName);
             }
             else
             {
-               
-                MessageBox.Show("Le répertoire source précisé est vide");
+
+                MessageBox.Show(lang.printEmptySourceDir);
             }
 
         }
 
         private void tab1SaveWork_Loaded(object sender, RoutedEventArgs e)
         {
-            Projet.EasySave DisplayWorks = new Projet.EasySave();
+            test2.EasySave DisplayWorks = new test2.EasySave();
             tab1DataGrid.ItemsSource = DisplayWorks.displayWorks();
         }
 
-        
+
 
         private void tab1DataGrid_LoadingRow(object sender, DataGridRowEventArgs e)
         {
@@ -316,7 +304,7 @@ namespace test2
 
         private void tab2DataGrid_Loaded(object sender, RoutedEventArgs e)
         {
-            Projet.EasySave DisplayWorks = new Projet.EasySave();
+            test2.EasySave DisplayWorks = new test2.EasySave();
             tab2DataGrid.ItemsSource = DisplayWorks.displayWorks();
         }
 
@@ -325,8 +313,30 @@ namespace test2
             e.Row.Header = (e.Row.GetIndex() + 1).ToString();
         }
 
-        private void tab1DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void tab3ButtonConfigureBuisnessSoftware_Click(object sender, RoutedEventArgs e)
         {
+            //here you just have to change log.json with the file you want tu put buisness software
+            // OpenProcess.OpenProcessFunction("notepad.exe", @"..\..\..\Files\log.json");
+            // Create OpenFileDialog
+            Ookii.Dialogs.Wpf.VistaFolderBrowserDialog openDlg = new Ookii.Dialogs.Wpf.VistaFolderBrowserDialog();
+
+            // Launch OpenFileDialog by calling ShowDialog method
+
+            Nullable<bool> result = openDlg.ShowDialog();
+
+            // Get the selected file name and display in a TextBox.
+            // Load content of file in a TextBlock
+            // Load content of file in a TextBlock
+
+
+        }
+
+        private void tab3ButtonConfigureTypeFilesToEncrypt_Click(object sender, RoutedEventArgs e)
+        {
+            OpenProcess.OpenProcessFunction("notepad.exe", @"..\..\..\Files\extensions.json");
+
+            //here you just have to change log.json with the file you want tu put the extensions for the files to encrypt
+
 
         }
     }
